dc5815c858e3681c485a97b95cc5f948759072f8
aug183 <vincent.nicol43@gmail.com>
2023-07-02 01:13:31 +0800

    Database updates. Important to reupdate db.

--- database/reservation.sql
+++ database/reservation.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 20, 2023 at 12:20 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 5.6.40
+-- Generation Time: Jul 01, 2023 at 07:13 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `admin` (
   `username` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `admin`
@@ -39,6 +38,37 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`username`, `password`) VALUES
 ('admin', '$2y$10$PIy2D/wlAALzU5IyZflau..zRH2J0inbAe1d45VWJ7yZJWIGCU40.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cancellations`
+--
+
+CREATE TABLE `cancellations` (
+  `reservation_code` int(11) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `date_created` datetime NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `contact` varchar(11) NOT NULL,
+  `organization` varchar(50) NOT NULL,
+  `services` varchar(50) NOT NULL,
+  `date_reserved` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `purpose` varchar(100) NOT NULL,
+  `reason` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `cancellations`
+--
+
+INSERT INTO `cancellations` (`reservation_code`, `password`, `last_name`, `first_name`, `date_created`, `email`, `contact`, `organization`, `services`, `date_reserved`, `start_time`, `end_time`, `purpose`, `reason`) VALUES
+(15, '^G&(@h9%AokXPMHl', 'Amurao', 'Sandra Mae', '2023-07-01 11:18:39', 'sandra_mae_amurao@dlsl.edu.ph', '09999999999', 'DLSL JPIA', 'Meeting Room 1', '2023-07-04', '07:00:00', '09:00:00', 'Meeting for thesis', 'Cancelled by Admin'),
+(16, 'yw9mbQsZIJ77D123', 'Nicol', 'Vincent', '2023-07-01 22:05:29', 'vincent_nino_nicol@dlsl.edu.ph', '09054172017', 'DLSL EDUCCIRCLE', 'Working Area', '2023-07-03', '08:00:00', '10:00:00', 'Software Engineering Presentation', 'Wrong Date');
 
 -- --------------------------------------------------------
 
@@ -52,17 +82,17 @@ CREATE TABLE `dates` (
   `Working area` varchar(20) NOT NULL DEFAULT 'AVAILABLE',
   `Meeting Room 1` varchar(20) NOT NULL DEFAULT 'AVAILABLE',
   `Meeting Room 2` varchar(20) NOT NULL DEFAULT 'AVAILABLE',
-  `Open Space` varchar(20) NOT NULL DEFAULT 'AVAILABLE',
+  `Open Space Near Lavoxa` varchar(20) NOT NULL DEFAULT 'AVAILABLE',
   `North Wing` varchar(20) NOT NULL DEFAULT 'AVAILABLE',
   `South Wing` varchar(20) NOT NULL DEFAULT 'AVAILABLE',
   `Zoom Account` varchar(20) DEFAULT 'AVAILABLE'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `dates`
 --
 
-INSERT INTO `dates` (`Dates`, `Reason`, `Working area`, `Meeting Room 1`, `Meeting Room 2`, `Open Space`, `North Wing`, `South Wing`, `Zoom Account`) VALUES
+INSERT INTO `dates` (`Dates`, `Reason`, `Working area`, `Meeting Room 1`, `Meeting Room 2`, `Open Space Near Lavoxa`, `North Wing`, `South Wing`, `Zoom Account`) VALUES
 ('2023-06-20', 'Holiday', 'UNAVAILABLE', 'UNAVAILABLE', 'UNAVAILABLE', 'UNAVAILABLE', 'UNAVAILABLE', 'UNAVAILABLE', 'UNAVAILABLE');
 
 -- --------------------------------------------------------
@@ -74,7 +104,7 @@ INSERT INTO `dates` (`Dates`, `Reason`, `Working area`, `Meeting Room 1`, `Meeti
 CREATE TABLE `offers` (
   `index_offers` int(11) NOT NULL,
   `offer_name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `offers`
@@ -98,7 +128,7 @@ INSERT INTO `offers` (`index_offers`, `offer_name`) VALUES
 CREATE TABLE `organizations` (
   `Acronym` varchar(16) NOT NULL,
   `Organization Name` varchar(88) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `organizations`
@@ -154,9 +184,10 @@ INSERT INTO `organizations` (`Acronym`, `Organization Name`) VALUES
 
 CREATE TABLE `reservations` (
   `reservation_code` int(11) NOT NULL,
+  `password` varchar(16) NOT NULL,
   `last_name` varchar(20) NOT NULL,
   `first_name` varchar(50) NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `email` varchar(50) NOT NULL,
   `contact` varchar(11) NOT NULL,
   `organization` varchar(50) NOT NULL,
@@ -165,17 +196,15 @@ CREATE TABLE `reservations` (
   `start_time` time NOT NULL,
   `end_time` time NOT NULL,
   `purpose` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `reservations`
 --
 
-INSERT INTO `reservations` (`reservation_code`, `last_name`, `first_name`, `date_created`, `email`, `contact`, `organization`, `services`, `date_reserved`, `start_time`, `end_time`, `purpose`) VALUES
-(4, 'Amurao', 'Sandra Mae', '2023-05-16 22:48:40', 'sandra_mae_amurao@dlsl.edu.ph', '09000000000', 'DLSL BIOS', 'Meeting Room 1', '2023-05-17', '12:00:00', '14:00:00', 'asd'),
-(7, 'Nicol', 'Vincent', '2023-05-17 10:40:51', 'vincent_nino_nicol@dlsl.edu.ph', '09000000000', 'DLSL ACES', 'Meeting Room 1', '2023-05-18', '22:40:00', '10:40:00', 'asd'),
-(9, 'Amurao', 'Sandra Mae', '2023-06-20 11:31:47', 'sandra_mae_amurao@dlsl.edu.ph', '09000000000', 'DLSL DEBSOC', 'South Wing', '2023-05-17', '12:00:00', '14:00:00', 'Practice'),
-(10, 'Nicol', 'Vincent', '2023-06-20 11:35:22', 'vincent_nino_nicol@dlsl.edu.ph', '09999999999', 'DLSL ANIMACO', 'Working Area', '2023-06-21', '08:00:00', '12:00:00', 'Practice');
+INSERT INTO `reservations` (`reservation_code`, `password`, `last_name`, `first_name`, `date_created`, `email`, `contact`, `organization`, `services`, `date_reserved`, `start_time`, `end_time`, `purpose`) VALUES
+(17, 'wvRkRxYQLJ9PETSO', 'Nicol', 'Vincent', '2023-07-02 00:50:26', 'vincent_nino_nicol@dlsl.edu.ph', '09054172017', 'DLSL JPCS', 'Zoom Account', '2023-07-05', '14:00:00', '16:00:00', 'EnCode Session 1'),
+(18, 'cDGSW3Tt2JHFxKtj', 'Faz', 'Faith Christine', '2023-07-02 01:09:11', 'faith_christine_faz@dlsl.edu.ph', '09999999999', 'DLSL JPCS', 'Open Space Near Lavoxa', '2023-07-06', '08:00:00', '10:00:00', 'Prop making for JPCS Nite');
 
 --
 -- Indexes for dumped tables
@@ -186,6 +215,13 @@ INSERT INTO `reservations` (`reservation_code`, `last_name`, `first_name`, `date
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `cancellations`
+--
+ALTER TABLE `cancellations`
+  ADD PRIMARY KEY (`reservation_code`),
+  ADD KEY `reservation_code` (`reservation_code`);
 
 --
 -- Indexes for table `dates`
@@ -218,6 +254,12 @@ ALTER TABLE `reservations`
 --
 
 --
+-- AUTO_INCREMENT for table `cancellations`
+--
+ALTER TABLE `cancellations`
+  MODIFY `reservation_code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
 -- AUTO_INCREMENT for table `offers`
 --
 ALTER TABLE `offers`
@@ -227,7 +269,7 @@ ALTER TABLE `offers`
 -- AUTO_INCREMENT for table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `reservation_code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `reservation_code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
